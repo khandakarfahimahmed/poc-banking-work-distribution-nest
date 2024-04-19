59b2d0796709acc96feca2487bd792a02093070a
khandakarfahimahmed <fahim363501@gmail.com>
2024-04-19 20:01:44 +0600

    workOrder distribution methods added in reviwerWorkOrder service

--- server/src/reviewer-work-order/reviewer-work-order.service.ts
+++ server/src/reviewer-work-order/reviewer-work-order.service.ts
@@ -35,4 +35,42 @@ export class ReviewerWorkOrderService {
   async findAllWorkOrder(): Promise<ReviewerWorkOrder[]> {
     return this.reviewerWorkOrderModel.findAll();
   }
+  async assignTask(workOrder_id: number, employee_id: number): Promise<void> {
+    try {
+      await this.workFlowAssignLogModel.create({
+        work_order_id: workOrder_id,
+        field_data_id: null,
+        assigned_to: employee_id,
+      });
+      console.log(`Task ${workOrder_id} assigned to ${employee_id}`);
+    } catch (error) {
+      console.log(
+        `Error assigning task ${workOrder_id} to ${employee_id}:`,
+        error,
+      );
+    }
+  }
+  async updateReviewerWorkOrder(
+    id: number,
+    status: string,
+    assigned_to: number,
+  ): Promise<void> {
+    try {
+      await this.reviewerWorkOrderModel.update(
+        {
+          status: status,
+          assigned_to: assigned_to,
+          start_time: new Date(),
+          isAssigned: true,
+        },
+        { where: { id } },
+      );
+      console.log(
+        `work order updated for task ${id} with assigned_to ${assigned_to}`,
+      );
+    } catch (error) {
+      console.log(`Error updating work order for task ${id}:`, error);
+      throw error;
+    }
+  }
 }
